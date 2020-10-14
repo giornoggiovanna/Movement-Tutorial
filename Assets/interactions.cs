a73f5e7780e3e8e88cfd69ef459d2f0a7c85cdd2
giornogiovanna <hayden.zsm@outlook.com>
2020-10-14 20:14:27 +0200

    My "finished" product; will be using this for further tutorials.

---
+++ Assets/interactions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactions : MonoBehaviour
+{  
+    void OnTriggerEnter2D(Collider2D other) {
+        print ("Enter");
+
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        print ("Exit");    
+    }
+
+
+    void OnTriggerStay2D(Collider2D other) {
+        print ("MUDA");    
+    }
+}
